cbff916bbfdad4c1c644f26a82b00c4b77865685
Rodrigo Avelões <68348382+rodrigoaveloes@users.noreply.github.com>
2021-08-15 12:05:49 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -49,7 +49,7 @@ export function Home() {
 
   return (
     <div id="page-auth">
-      <aside>
+      <aside className="auth">
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
